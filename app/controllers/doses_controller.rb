9ec4e8b3917afbfa27c0d23efdfcb8ff1bca6cf0
azadeharam <aram.azadeh@yahoo.com>
2020-03-29 16:58:44 +0200

    the views are completed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,26 +1,32 @@
 class DosesController < ApplicationController
 
-  def create
+  def new
+    @dose = Dose.new
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.all
+  end
+
+  def create
     @dose = Dose.new(dose_params)
-    @dose.cocktail = @cocktail
-    if @dose.save
-      redirect_to cocktail_doses_path(@dose.cocktail)
-    else
-      @dose = Dose.new
-      render 'new'
-    end
+    @dose.cocktail = find_cocktail
+    @dose.ingredient = Ingredient.find(dose_params[:ingredient_id])
+    @dose.save
+    redirect_to cocktail_path(params[:cocktail_id])
   end
 
   def destroy
-   @dose = Dose.find(params[:id])
-   @dose.destroy
-   redirect_to cocktail_path(@dose.cocktail)
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(params[:cocktail_id])
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :cocktail_id)
+    params.require(:dose).permit(:id, :description, :ingredient_id, :cocktail_id)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
