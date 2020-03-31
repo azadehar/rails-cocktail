757492520197bbf6c2cd1afd9b8500dfda55325d
azadeharam <aram.azadeh@yahoo.com>
2020-03-31 15:57:26 +0200

    The show page has been updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "cocktails#index"
-    resources :cocktails, only: [:index, :show, :new, :create] do
-      resources :doses, only: [:new, :create]
-      resources :ingredients, only: [:new, :create]
+    resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+      resources :doses, only: [:new, :create, :destroy]
+      resources :ingredients, only: [:new, :create, :destroy]
     end
     resources :doses, only: [:destroy]
 end
